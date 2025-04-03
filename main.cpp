02ce506a4d2810cf1e0fbf58af8747b4b45dbbc7
CyberNinja2500 <cyber.ninja2500@gmail.com>
2025-04-03 13:50:37 -0700

    First code generated

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 int main() {
-    std::cout << "Hello and welcome to " << lang << "!\n";
+    std::cout << "Hello world";
 
     return 0;
 }
